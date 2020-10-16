7fce8b4c655b99b68e7484bebff8c957a794bb75
Eugene Shumilin <shumilinea@gmail.com>
2020-10-16 22:43:49 +0300

    Add flag and test

--- hw1/cmd/fibonacci/main.go
+++ hw1/cmd/fibonacci/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"go-hardcode/hw1/pkg/fibonacci"
 )
 
 func main() {
-
-	for n := -1; n < 25; n ++ {
-		f, err := fibonacci.Fib(n)
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(f)
+	n := flag.Int("n", 0, "fib number")
+	flag.Parse()
+	if *n > 20 {
+		fmt.Println(errors.New("value cannot be greater than 20"))
+		return
 	}
+
+	f, err := fibonacci.Fib(*n)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(f)
 }
